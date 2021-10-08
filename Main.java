39afa03b665c324a3173954488b43dd20e8e344c
maciejszulia <maciej.szulia@gmail.com>
2021-10-08 14:55:36 +0200

    initial commit

---
+++ Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+        // zadanie 1
+        System.out.println(31 + 29 + 31);
+        // zadanie 2
+        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10);
+        // zadanie 3
+        System.out.println(1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10);
+        // zadanie 4
+        double a;
+        double b;
+        double c;
+        double d;
+        a = 1000;
+        b = a * 0.06 + a;
+        c = b * 0.06 + b;
+        d = c * 0.06 + c;
+        System.out.println("Saldo po pierwszym roku: " + b);
+        System.out.println("Saldo po pierwszym roku: " + c);
+        System.out.println("Saldo po pierwszym roku: " + d);
+        // zadanie 5
+        System.out.println("+------+");
+        System.out.println("| Java |");
+        System.out.println("+------+");
+        // zadanie 6
+        System.out.println("  /////  ");
+        System.out.println(" +'''''+ ");
+        System.out.println("(| o o |)");
+        System.out.println("  | ^ |  ");
+        System.out.println(" | '-' | ");
+        System.out.println(" +-----+" );
+
+        // zadanie 7
+
+
+
+    }
+}
