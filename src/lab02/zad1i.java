5cb2a01c0d620eb63c25a970c9640bcaed48fac8
maciejszulia <maciej.szulia@gmail.com>
2021-10-15 20:53:41 +0200

    added zad1i

---
+++ src/lab02/zad1i.java
@@ -0,0 +1,47 @@
+package lab02;
+
+import java.util.Scanner;
+
+public class zad1i {
+
+    public static int skanerDlaLab02() {
+        Scanner skaner = new Scanner(System.in);
+        System.out.print("Podaj liczbe: ");
+        int n = skaner.nextInt();
+        System.out.println("n = " + n);
+        return n;
+    }
+
+    public static long silnia(int n) {
+        long silnia = 1;
+        for (int i = 2; i <= n; i++) {
+            silnia = silnia * i;
+        }
+        return silnia;
+    }
+
+    public static double dzialanie(int n) {
+        double wynik = 0, temp = 1;
+        Scanner scannerDlaOdejmowania = new Scanner(System.in);
+        for (int i = 0; i < n; i++) {
+            double liczba = scannerDlaOdejmowania.nextDouble();
+            if (i % 2 == 0) {
+                wynik -= liczba/(double) silnia(i+1);
+                temp = liczba;
+            } else {
+                wynik += liczba/(double) silnia(i+1);
+                temp = liczba;
+            }
+        }
+        System.out.println("--endOfFunc--");
+        return wynik + (Math.pow(-1, n + 1) * temp)/silnia(n);
+    }
+
+    public static void main(String[] args) {
+
+        int n = skanerDlaLab02();
+        double wynikDzialania = dzialanie(n);
+
+        System.out.println("Wynik dzialania z podpunktu i = " + wynikDzialania);
+    }
+}
