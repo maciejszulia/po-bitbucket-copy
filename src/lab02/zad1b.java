ff0459ff7241cc365c55671682cacb8224416e06
maciejszulia <maciej.szulia@gmail.com>
2021-10-15 14:36:00 +0200

    todo

--- src/lab02/zad1b.java
+++ src/lab02/zad1b.java
@@ -11,15 +11,15 @@ public class zad1b {
         System.out.println("Podaj liczbę: ");
 
         //liczba = zeskanuj kolejnego inta
-        int liczba = scanner.nextInt();
-        System.out.println("n = " + liczba);
+        int n = scanner.nextInt();
+        System.out.println("n = " + n);
 
         double wynik = 1;
-        double[] tabLiczby = new double[liczba];
+        double[] tabLiczby = new double[n];
         for (int i = 0; i < tabLiczby.length; i++) {
             tabLiczby[i] = scanner.nextDouble();
             wynik *= tabLiczby[i];
-            System.out.println(wynik);  //mozna to zrobic inaczej: wynik *= liczba;
+            System.out.println(wynik);  //todo: mozna to zrobic inaczej: wynik *= liczba;
         }
 
         System.out.println("wynik = " + wynik);
