7849a45dd9e91a2375c147eb3eef29dd56755205
maciejszulia <maciej.szulia@gmail.com>
2021-10-15 14:24:36 +0200

    zad1-c

--- src/lab02/zad1c.java
+++ src/lab02/zad1c.java
@@ -1,10 +1,21 @@
 package lab02;
 
 import java.util.Scanner;
+import java.lang.Math;
 
 public class zad1c {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbę: ");
+        int n = scanner.nextInt();
+        System.out.println("n = " + n);
 
+        double wynik = 0;
+        for (int i = 0; i < n; i++) {
+            double liczba = scanner.nextDouble();
+            wynik += Math.abs(liczba);
+        }
+        System.out.println("wynik = " + wynik);
     }
 }
