a9b434d51e92d6c8293a2d6a21296bd23bbbe6dd
maciejszulia <maciej.szulia@gmail.com>
2021-10-15 14:35:50 +0200

    zad1-e

---
+++ src/lab02/zad1e.java
@@ -0,0 +1,23 @@
+package lab02;
+
+import java.util.Scanner;
+
+public class zad1e {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbę: ");
+        int n = scanner.nextInt();
+        System.out.println("n = " + n);
+
+        double wynik = 1;
+        double[] tabLiczby = new double[n];
+        for (int i = 0; i < tabLiczby.length; i++) {
+            tabLiczby[i] = scanner.nextDouble();
+            wynik *= Math.abs(tabLiczby[i]);
+            System.out.println(wynik);  //todo: mozna to zrobic inaczej: wynik *= liczba;
+        }
+
+        System.out.println("wynik = " + wynik);
+    }
+}
