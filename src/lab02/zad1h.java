5e04da41e7469b5df3af34e26c47375e79320c4b
maciejszulia <maciej.szulia@gmail.com>
2021-10-15 15:29:25 +0200

    added mnozenie todo: add skaner func.

---
+++ src/lab02/zad1h.java
@@ -0,0 +1,25 @@
+package lab02;
+
+import java.util.Scanner;
+
+public class zad1h {
+    public static void main(String[] args) {
+
+        //init scanner
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbę: ");
+
+        int n = scanner.nextInt();
+        System.out.println("n = " + n);
+
+        double wynik = 0;
+        for (int i = 0; i < n; i++) {
+            double liczba = scanner.nextDouble();
+            wynik += liczba;
+            System.out.println(wynik);
+        }
+        System.out.println("wynik = " + wynik);
+    }
+}
+
+//jest dobrze
