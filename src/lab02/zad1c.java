79c35cf94fca08f83a7f3cbf29e8bcb1b2b362c8
maciejszulia <maciej.szulia@gmail.com>
2021-10-15 17:52:31 +0200

    added mnozenieAbs func.

--- src/lab02/zad1c.java
+++ src/lab02/zad1c.java
@@ -4,19 +4,32 @@ import java.util.Scanner;
 import java.lang.Math;
 
 public class zad1c {
+
+    public static int skanerDlaLab02() {
+        Scanner skaner = new Scanner(System.in);
+        System.out.print("Podaj liczbe: ");
+        int n = skaner.nextInt();
+        System.out.println("n = " + n);
+        return n;
+    }
+
+    public static double mnozenieAbs(int n) {
+        double wynik = 1;
+        Scanner scannerDlaMnozenia = new Scanner(System.in);
+        for (int i = 0; i < n; i++) {
+            double liczba = scannerDlaMnozenia.nextDouble();
+            wynik *= Math.abs(liczba);
+        }
+        System.out.println("--endOfFunc--");
+        return wynik;
+    }
+
+
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj liczbę: ");
+        int n = skanerDlaLab02();
+        double wynikMnozeniaAbs = mnozenieAbs(n);
 
-        int n = scanner.nextInt();
-        System.out.println("n = " + n);
-
-        double wynik = 0;
-        for (int i = 0; i < n; i++) {
-            double liczba = scanner.nextDouble();
-            wynik += Math.abs(liczba);
-        }
-        System.out.println("wynik = " + wynik);
+        System.out.println("Wynik mnozenia z wartosci bezwzglednych = " + wynikMnozeniaAbs);
     }
 }
