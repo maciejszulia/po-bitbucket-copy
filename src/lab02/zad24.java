803bfe5cd68beb2f83d135f9f174705c4db54835
maciejszulia <maciej.szulia@gmail.com>
2021-10-22 14:44:13 +0200

    zad 2-4

---
+++ src/lab02/zad24.java
@@ -0,0 +1,35 @@
+package lab02;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class zad24 {
+    public static int skanerDlaLab02() {
+        Scanner skaner = new Scanner(System.in);
+        System.out.print("Podaj liczbe: ");
+        int n = skaner.nextInt();
+        System.out.println("n = " + n);
+        return n;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner skaner = new Scanner(System.in);
+        int n = skanerDlaLab02();
+        double[] tablica = new double[n];
+
+        int i = 0;
+        double smallest = tablica[0], biggest = tablica[0];
+        while (i != tablica.length) {
+            tablica[i] = skaner.nextDouble();
+            if (tablica[i] < smallest)
+                smallest = tablica[i];
+            if (tablica[i] > biggest)
+                biggest = tablica[i];
+            i++;
+        }
+        System.out.println("Najmniejsza = " + smallest + " najwieksza = " + biggest);
+    }
+
+}
+
