b8ccedf6a1a123b276643e5c448ec061467243fc
maciejszulia <maciej.szulia@gmail.com>
2021-12-03 15:33:25 +0100

    jakies pierdoly

---
+++ src/lab05/zad2.java
@@ -0,0 +1,28 @@
+package lab05;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class zad2 {
+
+    static int countCharFromFile(String filePath, char c) throws FileNotFoundException {
+        int counter = 0;
+        Scanner sc = new Scanner(new File(filePath));
+        while (sc.hasNext()) {
+            String str = sc.next();
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == c) {
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        System.out.println(countCharFromFile("C:\\Users\\macie\\IdeaProjects\\programowanie-obiektowe\\src\\lab05\\file",'a'));
+
+    }
+}
